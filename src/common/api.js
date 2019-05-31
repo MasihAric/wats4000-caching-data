a38502127138ebf42dc60acf40a02c826b9768b6
Aric Masih <masiharic@seattleu.edu>
2019-05-31 13:29:07 -0700

    Completed caching project and removed todos

--- src/common/api.js
+++ src/common/api.js
@@ -10,7 +10,7 @@ API.interceptors.request.use(function (config) {
     // Set common parameters for each request to the API.
 
     // TODO: Use your own APPID for OpenWeatherMap.org
-    config.params.APPID = 'YOUR_APPID_HERE';
+    config.params.APPID = 'd00aeab1589e0e782e300858e19669e2';
     config.params.units = 'imperial';
 
     return config;
